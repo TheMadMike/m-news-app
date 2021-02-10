b1f79a7ca1dd266925d922e9b6baef0b6c7b8a4a
TheMadMike <m.gibas2407@gmail.com>
2021-02-10 19:13:47 +0100

    Create Footer component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="font-body w-full md:w-3/4 lg:w-3/4 mx-auto bg-gray-50 min-h-screen">
       <Header/>
+      <main className="min-h-screen">
+
+      </main>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+const footerLinks = [
+    {href: '#', text: 'GitHub repository'},
+    {href: 'https://newsapi.org/', text: 'News API'},
+    {href: 'http://mgibas.me/', text: 'Developer'},
+];
+
+const Footer = () => {
+    return (
+        <footer className="flex w-full h-40 bg-gray-900 border-t-8 border-gray-600 justify-between text-xs md:text-sm text-gray-50">
+            <div className="px-6 my-auto flex-col">
+                { footerLinks.map((link) => {
+                    return <a className="transition duration-500 my-6 lg:my-2 block underline hover:text-gray-300" href={link.href}> {link.text} </a>;
+                })}
+            </div>
+            <div className="px-6 my-auto">
+                Copyright &copy; 2021 Michał Gibas. All rights reserved.
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
