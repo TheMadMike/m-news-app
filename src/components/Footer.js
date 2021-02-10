4081356df593d8407e0bbd71d8c027793a22b107
TheMadMike <m.gibas2407@gmail.com>
2021-02-10 23:36:52 +0100

    Fix some stylistic problems with footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,11 +8,11 @@ const Footer = () => {
     return (
         <footer className="flex w-full h-40 bg-gray-900 border-t-8 border-gray-600 justify-between text-xs md:text-sm text-gray-50">
             <div className="px-6 my-auto flex-col">
-                { footerLinks.map((link) => {
-                    return <a className="transition duration-500 my-6 lg:my-2 block underline hover:text-gray-300" href={link.href}> {link.text} </a>;
+                { footerLinks.map((link, key) => {
+                    return <a key={key} className="transition duration-500 my-6 lg:my-2 block underline hover:text-gray-300" href={link.href}> {link.text} </a>;
                 })}
             </div>
-            <div className="px-6 my-auto">
+            <div className="px-2 md:px-6 my-auto">
                 Copyright &copy; 2021 Michał Gibas.
             </div>
         </footer>
