8d8d131a806e13c39a596dd33bce755dca114398
TheMadMike <m.gibas2407@gmail.com>
2021-02-11 00:07:34 +0100

    Create an interface for fetching data from the API

---
+++ src/Api.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+const makeGetRequest = async (uri) => {
+    try {
+        const response = await axios.get(uri);
+        return response.data;
+    } catch(error) {
+        console.error(error);
+        return null;
+    }
+};
+
+const constructUri = (base, query) => {
+    let uri = base;
+    
+    for(const p in query) {
+        if(query.hasOwnProperty(p)) {
+            uri += `?${p}=${query[p]}&`;
+        }
+    }
+    
+    return uri;
+};
+
+const getTopHeadlines = async (options) => {
+    const uri = constructUri('top-headlines', options);
+    return await makeGetRequest(uri);
+};
+
+const getEverything = async (options) => {
+    const uri = constructUri('everything', options);
+    return await makeGetRequest(uri);
+}
+
+const getAllSources = async (options) => {
+    const uri = constructUri('sources', options);
+    return await makeGetRequest(uri);
+};
+
+export default { getAllSources, getTopHeadlines, getEverything };
